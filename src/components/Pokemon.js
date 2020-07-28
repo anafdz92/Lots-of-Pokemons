27d965b0d40d0c1e5fed514b8467963d094b4a13
Ana Fernandez <anafdz92@gmail.com>
2020-07-28 09:56:12 +0200

    add pokemon to the List

--- src/components/Pokemon.js
+++ src/components/Pokemon.js
@@ -1,13 +1,16 @@
 import React from "react";
 
 const Pokemon = (props) => {
+  const pokeTypes = props.type.map((typeEach, index) => {
+    return <li key={index}>{typeEach}</li>;
+  });
   return (
     <article>
       <div>
         <img src={props.url} />
       </div>
       <h2>{props.name}</h2>
-      <p>{props.type}</p>
+      <ul>{pokeTypes}</ul>
     </article>
   );
 };

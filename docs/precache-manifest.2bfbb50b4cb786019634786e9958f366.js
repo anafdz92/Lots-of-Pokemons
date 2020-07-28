376a5632961bfd38553995b5391b345c87efd02c
Ana Fernandez <anafdz92@gmail.com>
2020-07-28 08:54:55 +0200

    add docs

---
+++ docs/precache-manifest.2bfbb50b4cb786019634786e9958f366.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca48f973674c6a8e0e903e5269a15132",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3788dd95357404788ad5",
+    "url": "./static/js/2.5bf06b29.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.5bf06b29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03efbb8ce002083c380f",
+    "url": "./static/js/main.13813862.chunk.js"
+  },
+  {
+    "revision": "e21397ecf58f77891282",
+    "url": "./static/js/runtime-main.8ee73f49.js"
+  }
+]);
